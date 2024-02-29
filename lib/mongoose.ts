7d5086c2c75d5e3977372bf13273c56f4e18117a
Sarvesh <sarveshjaiofficial@gmail.com>
2024-03-01 03:19:19 +0530

    added

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -4,7 +4,6 @@ let isConnected = false;
 
 export const connectToDB = async () => {
   mongoose.set("strictQuery", true);
-
   if (!process.env.MONGODB_URI)
     return console.log("MONGODB_URI is not defined");
 
